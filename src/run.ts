5c47b728189603d9738fdbc193749f3027c6afeb
h-a-n-a <andywangsy@gmail.com>
2022-03-17 15:05:19 +0800

    fix: fix clean up for benchmarks

--- src/run.ts
+++ src/run.ts
@@ -95,7 +95,7 @@ const setupFixtureBenchmarks = async (opts: {
       }, Promise.resolve())
   }
 
-  return { linkedDeps, tmpBenchmarkDir }
+  return { linkedDeps, tmpBenchmarkDir, tmpBenchmarkRootDir }
 }
 
 const cleanupBenchmarks = async (tmpDir: string, linkedDeps: {
@@ -105,6 +105,12 @@ const cleanupBenchmarks = async (tmpDir: string, linkedDeps: {
   for (const { pkgName, directory } of linkedDeps) {
     console.log(`Unlinking ${pkgName} for ${directory}`)
     await yarnUnlink(directory, pkgName)
+  }
+
+  if (isDebug) {
+    console.log('Debug mode, skip cleaning up benchmark root dir...', tmpDir)
+  } else {
+    await fs.remove(tmpDir)
   }
 }
 
@@ -117,7 +123,7 @@ const runFixtureBenchmarks = async <T extends {
 }): Promise<FixtureBenchmark[]> => {
   const { plugins, speedyPackages, benchmarkConfig } = opts
 
-  const { linkedDeps, tmpBenchmarkDir } = await setupFixtureBenchmarks({
+  const { linkedDeps, tmpBenchmarkDir, tmpBenchmarkRootDir } = await setupFixtureBenchmarks({
     benchmarkDir: benchmarkConfig.directory,
     speedyPackages
   })
@@ -149,7 +155,7 @@ const runFixtureBenchmarks = async <T extends {
   }
 
   // Do some cleanups
-  await cleanupBenchmarks(tmpBenchmarkDir, linkedDeps)
+  await cleanupBenchmarks(tmpBenchmarkRootDir, linkedDeps)
 
   return fixtureBenchmarks
 }
