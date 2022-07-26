dbdbac87fefb1579cef5f1984365e14a2aae4c58
h-a-n-a <andywangsy@gmail.com>
2022-07-26 18:59:57 +0800

    feat: take down plugin node modules size

--- src/performance-plugins/speedy/index.ts
+++ src/performance-plugins/speedy/index.ts
@@ -1,5 +1,4 @@
-import { NodeModulesPlugin } from './node-modules'
 import { BundleSizePlugin } from './bundle-size'
 import { RequireCost } from './require-cost'
 
-export const speedyPlugins = [RequireCost, NodeModulesPlugin, BundleSizePlugin]
+export const speedyPlugins = [RequireCost, BundleSizePlugin]
